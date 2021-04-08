d154a75e25b715cd17688d640d61d2eceff170aa
kevin-salamon <kevinwebdevs@gmail.com>
2021-04-08 18:54:07 -0400

    added new build source to POM

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,14 +1,27 @@
 package clinic.programming.training;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     
     public Application() {
-        System.out.println ("Inside Application");
+        System.out.println("Inside Application");
     }
 
-    // method main(): ALWAYS the APPLICATION entry point
+    public void greet() {
+        ArrayList<String> greetings = new ArrayList<String>();
+        greetings.add("Hello");
+
+        for (String greeting : greetings) {
+            System.out.println("Greetings" + greeting);
+        }
+
+    }
+
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	    Application app = new Application();
+        app.greet();
     }
 }

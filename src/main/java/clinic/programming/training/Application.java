77a0886e6fb8e35e11ec9eeb4e7e44fa80030f9c
kevin-salamon <kevinwebdevs@gmail.com>
2021-04-12 19:19:10 -0400

    learn to add dependencies in maven

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,12 +1,18 @@
 package clinic.programming.training;
 
 import java.util.ArrayList;
+import org.apache.commons.lang3.StringUtils;
 import java.util.List;
 
 public class Application {
     
     public Application() {
         System.out.println("Inside Application");
+    }
+    
+    public int countWords(String words) {
+    	String[] separateWords = StringUtils.split(words, ' ');
+    	return (separateWords == null) ? 0 : separateWords.length;
     }
 
     public void greet() {
@@ -23,5 +29,7 @@ public class Application {
     	System.out.println ("Starting Application");
 	    Application app = new Application();
         app.greet();
+        int count = app.countWords("Let's test this out.");
+        System.out.println("COUNT: " + count);
     }
 }
